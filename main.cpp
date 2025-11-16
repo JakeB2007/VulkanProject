95581595054373faf5d40caf1045c76c87ed228e
Jake Bignell <Jake_240407@outlook.com>
2025-11-16 22:34:14 +0000

    more troubleshooting

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
+#include <GLFW/glfw3.h>
+
+#include "lib/glfw-3.4/src/internal.h"
 
 int main() {
     std::cout << "Hello World!" << std::endl;
     std::cout << "Hello World!" << std::endl;
 
+    int32_t hello = 2;
+    std::cout << hello << std::endl;
     return 0;
 }
